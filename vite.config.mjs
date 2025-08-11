8c3dfe35d7e8db0157d3aa8639d253fc3fd6ecd1
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-10 19:51:33 -0500

    chore: Configurar alias '@' del directorio ./src

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     host: true,
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
 });
