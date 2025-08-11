25c0dc916604f07d2903303a0f90f42ee0df4ad7
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-10 19:54:54 -0500

    feat: Crear componentes de paginas

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { HomePage } from "./home-page/HomePage";
+export { ProductsPage } from "./products-page/ProductsPage";
+export { AboutUsPage } from "./aboutus-page/aboutUsPage";
+export { TrackingPage } from "./tracking-page/TrackingPage";
+export { ContactPage } from "./contact-page/ContactPage";
---
+++ src/pages/tracking-page/TrackingPage.jsx
@@ -0,0 +1,3 @@
+export function TrackingPage() {
+  return <div>TrackingPage</div>;
+}
