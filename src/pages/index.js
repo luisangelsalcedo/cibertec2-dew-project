1eeaebfb9977fdaddb9a7e0a4e137faf9e593d07
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-10 23:42:33 -0500

    fix: Corregir importacion de pagina AboutUs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { HomePage } from "./home-page/HomePage";
 export { ProductsPage } from "./products-page/ProductsPage";
-export { AboutUsPage } from "./aboutus-page/aboutUsPage";
+export { AboutUsPage } from "./aboutus-page/AboutUsPage";
 export { TrackingPage } from "./tracking-page/TrackingPage";
 export { ContactPage } from "./contact-page/ContactPage";
