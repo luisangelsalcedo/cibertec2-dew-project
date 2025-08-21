f2ae7d331ad9f565ac18636817fbdfd7b01a8ae2
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-21 14:11:22 -0500

    T7 - Crear los components globales - MainTitle

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { MainTitle } from "./main-title/MainTitle";
 export { Header } from "./header/Header";
 export { Footer } from "./footer/Footer";
 export { Container } from "./container/Container";
---
+++ src/components/main-title/MainTitle.jsx
@@ -0,0 +1,5 @@
+import "./main-title.scss";
+
+export function MainTitle({ children }) {
+  return <h2 className="main-title">{children}</h2>;
+}
