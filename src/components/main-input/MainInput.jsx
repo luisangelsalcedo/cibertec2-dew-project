8eeb8e5c8e3ef86d7eda13a8c3cc6cb9b851f1b2
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-23 12:59:08 -0500

    fix: Agregando soporte para input file

--- src/components/main-input/MainInput.jsx
+++ src/components/main-input/MainInput.jsx
@@ -4,23 +4,28 @@ export function MainInput({ label, name, errorMessaje, element, ...props }) {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      {!element && (
-        <input
-          type={props.type || "text"}
-          id={name}
-          name={name}
-          className={errorMessaje ? "error" : null}
-          {...props}
-        />
-      )}
-      {element == "textarea" && (
-        <textarea
-          id={name}
-          name={name}
-          className={errorMessaje ? "error" : null}
-          {...props}
-        ></textarea>
-      )}
+      <div className={props.type == "file" ? "file" : null}>
+        {!element && (
+          <input
+            type={props.type || "text"}
+            id={name}
+            name={name}
+            className={errorMessaje ? "error" : null}
+            {...props}
+          />
+        )}
+        {props.type == "file" && (
+          <p>Permitido subir archivo en formato PDF máx. 1 MB</p>
+        )}
+        {element == "textarea" && (
+          <textarea
+            id={name}
+            name={name}
+            className={errorMessaje ? "error" : null}
+            {...props}
+          ></textarea>
+        )}
+      </div>
 
       <div className="error-message">{errorMessaje}</div>
     </div>
