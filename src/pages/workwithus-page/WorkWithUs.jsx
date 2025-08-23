78b7a9e2469f7aab4d5dbb0cacc6abaa39b72630
Sofia Garcia <josoaugar@gmail.com>
2025-08-23 12:49:28 -0500

    avance con formik

--- src/pages/workwithus-page/WorkWithUs.jsx
+++ src/pages/workwithus-page/WorkWithUs.jsx
@@ -1,133 +1,24 @@
-import React, {useState} from "react";
+import { Container } from "@/components";
+import WorkWithUsForm from "./WorkWithUsForm";
 import "./workwithus.scss";
+
 export function WorkWithUs() {
-  const [formData, setFormData] = useState({
-    nombre: "",
-    apellido: "",
-    correo: "",
-    telefono: "",
-    departamento: "",
-    distrito: "",
-    area: "",
-    cv: null,
-  });
-
-  const handleChange = (e) => {
-    const { name, value, files } = e.target;
-    setFormData({
-      ...formData,
-      [name]: files ? files[0] : value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Formulario enviado ✅");
-    console.log(formData);
-  };
-
   return (
+  <main>
     <div className="work-section">
-      {/* Título */}
-      <h2 className="title">
-        TRABAJA CON <span>NOSOTROS</span>
-      </h2>
+        <Container>
+          <h2>
+            TRABAJA CON <span className="highlight">NOSOTROS</span>
+          </h2>
+          <div className="flex">
+            <WorkWithUsForm />
 
-      <div className="work-content">
-        {/* Formulario */}
-        <form className="work-form" onSubmit={handleSubmit}>
-          <div className="row2">
-            <p>Nombres:</p>
-            <p>Apellidos:</p>
+            <div className="work-image">
+                <img src="./src/assets/images/trabajaconnosotros.png" alt="" />
+            </div>
           </div>
-          <div className="row">
-              <input
-                type="text"
-                name="nombre"
-                placeholder="Nombres"
-                value={formData.nombre}
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="apellido"
-                placeholder="Apellidos"
-                value={formData.apellido}
-                onChange={handleChange}
-              />
-            
-          </div>
-          <p>Correo:</p>
-          <input
-            type="email"
-            name="correo"
-            placeholder="email"
-            value={formData.correo}
-            onChange={handleChange}
-          />
-          <p>Telefono:</p>
-          <input
-            type="tel"
-            name="telefono"
-            placeholder="Teléfono"
-            value={formData.telefono}
-            onChange={handleChange}
-            />
-          <div className="row2">
-            <p>Departamento:</p>
-            <p>Distrito:</p>
-          </div>
-          <div className="row">
-            <input
-              type="text"
-              name="departamento"
-              placeholder="Departamento"
-              value={formData.departamento}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="distrito"
-              placeholder="Distrito"
-              value={formData.distrito}
-              onChange={handleChange}
-            />
-          </div>
-          <p>Área de Solicitud:</p>
-          <input
-            type="text"
-            name="area"
-            placeholder="Solicitud"
-            value={formData.area}
-            onChange={handleChange}
-          />
-        </form>
-        {/* Imagen */}
-        <div className="work-image">
-          <img src="./src/assets/images/trabajaconnosotros.png" alt="" />
-        </div>
+        </Container>
       </div>
-      <br />
-      <div className="cv-box">
-            <label className="cv-btn">
-              ADJUNTAR CV
-              <input
-                type="file"
-                name="cv"
-                accept=".pdf"
-                onChange={handleChange}
-                hidden
-              />
-            </label>
-            <p>Permitido subir archivo en formato PDF máx. 1 MB</p>
-            <br />
-            <button type="submit" className="submit-btn">
-              ENVIAR
-            </button>         
-         </div>
-
-          
-    </div>
-  );
-  return <>WorkWithUs</>;
+    </main>
+  )
 }
