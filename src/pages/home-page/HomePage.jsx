bc44e3b0ef002bbeca7292fb173347066bed58ca
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2025-08-24 12:13:32 -0500

    fix: Correccion del menu flotante del home

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,11 +1,12 @@
 import { MainSlider } from "./components/main-slider/MainSlider";
 import { PromoGrid } from "./components/promo-grid/PromoGrid";
+import "./home-page.scss";
 
 export function HomePage() {
   return (
     <div>
-        <MainSlider></MainSlider>
-        <PromoGrid></PromoGrid>
+      <MainSlider></MainSlider>
+      <PromoGrid></PromoGrid>
     </div>
   );
 }
